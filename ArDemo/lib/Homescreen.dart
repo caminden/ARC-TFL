872ae6cda8e003193abd5edfe9b73bd8e65d5c63
chase minden <caminden@gmail.com>
2021-03-24 21:15:33 -0500

    Implement camera capture and display

--- ArDemo/lib/Homescreen.dart
+++ ArDemo/lib/Homescreen.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
-import 'package:ArDemo/Camera.dart';
+import 'dart:typed_data';
+//import 'package:ArDemo/Camera.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
 import 'dart:math' as math;
 
 const String ssd = "SSDMobileNet";
@@ -25,10 +31,12 @@ class _HomeScreen extends State<HomeScreen> {
   int view = 0;
   _Controller con;
   CameraController controller;
-  XFile imageFile;
+  ScreenshotController screenshotController = ScreenshotController();
+  XFile ximageFile; //for camera FileImage(File(imageFile.path))))
   Offset offset = Offset(0, 100);
   Offset imageOffset = Offset(0, 100);
   Alignment imageAlign = Alignment(0, 0);
+  static GlobalKey previewContainer = new GlobalKey();
 
   @override
   void initState() {
@@ -52,104 +60,121 @@ class _HomeScreen extends State<HomeScreen> {
 
   void render(fn) => setState(fn);
 
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    con.arController.dispose();
+    super.dispose();
+  }
+
   //basic homescreen, no functionality as of now
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     Size screen = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter ArCore/Tflite Demo"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.adb),
-            onPressed: () {
-              setState(() {
-                view == 0 ? view = 1 : view = 0;
-                offset = Offset(0, 100);
-                imageFile = null;
-              });
-            },
-          )
-        ],
-      ),
-      body: view == 0
-          ? Container()
-          : Stack(
-              children: <Widget>[
-                con.Camera(),
-                Positioned(
-                  top: offset.dy - 90,
-                  left: offset.dx,
-                  child: Draggable(
-                    childWhenDragging: Container(),
-                    child: Container(
-                        height: 110,
-                        width: 110,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white))),
-                    feedback: Container(
-                        height: 110,
-                        width: 110,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white))),
-                    onDragEnd: (drag) {
-                      setState(() {
-                        offset = drag.offset;
-                        print(drag.offset);
-                      });
-                    },
+    return RepaintBoundary(
+      key: previewContainer,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter ArCore/Tflite Demo"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.adb),
+              onPressed: () {
+                setState(() {
+                  view == 0 ? view = 1 : view = 0;
+                  offset = Offset(0, 100);
+                  //imageFile = null;
+                });
+              },
+            )
+          ],
+        ),
+        body: view == 0
+            ? Container()
+            : Stack(
+                children: <Widget>[
+                  CameraPreview(controller), //to display the camera
+                  Positioned(
+                    top: offset.dy - 90,
+                    left: offset.dx,
+                    child: Draggable(
+                      childWhenDragging: Container(),
+                      child: Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue))),
+                      feedback: Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue))),
+                      onDragEnd: (drag) {
+                        setState(() {
+                          offset = drag.offset;
+                          Alignment n = con.normalize(drag.offset.dx, drag.offset.dy);
+                          print(drag.offset);
+                        });
+                      },
+                    ),
                   ),
-                ),
-                imageFile != null
-                    ? Positioned(
-                        top: imageOffset.dy - 90,
-                        left: imageOffset.dx,
-                        child: Draggable(
-                            childWhenDragging: Container(),
-                            child: Container(
-                              clipBehavior: Clip.hardEdge,
-                              height: 110,
-                              width: 110,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      scale: 1.75,
-                                      fit: BoxFit.none,
-                                      alignment: imageAlign,
-                                      image: FileImage(File(imageFile.path)))),
-                            ),
-                            feedback: Container(
-                              height: 110,
-                              width: 110,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      scale: 1.75,
-                                      fit: BoxFit.none,
-                                      alignment: imageAlign,
-                                      image: FileImage(File(imageFile.path)))),
-                            ),
-                            onDragEnd: (drag) {
-                              setState(() {
-                                imageOffset = drag.offset;
-                                print(drag.offset);
-                              });
-                            }),
-                      )
-                    : Container(),
-              ],
-            ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          XFile image = await controller.takePicture();
-          setState(() {
-            imageAlign = con.normalize(offset.dx, offset.dy);
-            imageFile = image;
-            imageOffset = offset;
-            offset = Offset(0, 100);
-            print(imageAlign.x);
-          });
-        },
-        child: Icon(Icons.photo_camera),
+                  ximageFile == null ? Container() : 
+                  Positioned(
+                    top: imageOffset.dy - 90,
+                    left: imageOffset.dx,
+                    child: Draggable(
+                        childWhenDragging: Container(),
+                        child: Container(
+                          clipBehavior: Clip.hardEdge,
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  scale: 2,
+                                  fit: BoxFit.none,
+                                  alignment: imageAlign,
+                                  image: FileImage(File(ximageFile.path)))),
+                        ),
+                        feedback: Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  scale: 2,
+                                  fit: BoxFit.none,
+                                  alignment: imageAlign,
+                                  image: FileImage(File(ximageFile.path)))),
+                        ),
+                        onDragEnd: (drag) {
+                          setState(() {
+                            imageOffset = drag.offset;
+                            print(drag.offset);
+                          });
+                        }),
+                  )
+                ],
+              ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            controller.takePicture().then((XFile image) {
+              print(
+                  "==============================================================");
+              print(
+                  "========================Picure Taken =========================");
+              setState(() {
+                ximageFile = image;
+                imageAlign = con.normalize(offset.dx, offset.dy);
+                imageOffset = offset;
+                offset = Offset(0, 100);
+              });
+              print(File(ximageFile.path).toString());
+              final result =
+                  ImageGallerySaver.saveFile(File(ximageFile.path).toString());
+            });
+          },
+          child: Icon(Icons.photo_camera),
+        ),
       ),
     );
   }
@@ -158,10 +183,13 @@ class _HomeScreen extends State<HomeScreen> {
 class _Controller {
   _HomeScreen _state;
   _Controller(this._state);
+  Map<String, ArCoreAugmentedImage> augmentedImageMap = Map();
+  ArCoreController arController;
+  Map<String, Uint8List> imageMap = Map();
 
   Widget Camera() {
     if (_state.controller == null || !_state.controller.value.isInitialized) {
-      return Container();
+      return Container(child: Text("Sample text"));
     }
 
     var tmp = MediaQuery.of(_state.context).size;
@@ -179,17 +207,92 @@ class _Controller {
           screenRatio > previewRatio ? screenH : screenW / previewW * previewH,
       maxWidth:
           screenRatio > previewRatio ? screenH / previewH * previewW : screenW,
-      child: CameraPreview(_state.controller),
+      child: ArCoreView(
+          onArCoreViewCreated: _arCoreView,
+          type: ArCoreViewType.AUGMENTEDIMAGES),
+      //CameraPreview(_state.controller),
     );
   }
 
+  _arCoreView(ArCoreController controller) {
+    arController = controller;
+    //controller.onTrackingImage = _handleTracking;
+    //loadImages();
+  }
+  /*
+  loadImages() async {
+    if(_state.imageFile != null){
+      imageMap["${_state.imageFile.name}"] = await _state.imageFile.readAsBytes();
+      arController.loadMultipleAugmentedImage(bytesMap: imageMap);
+    }
+  }
+
+  _handleTracking(ArCoreAugmentedImage augImage){
+    if(!augmentedImageMap.containsKey(augImage.name)){
+      augmentedImageMap[augImage.name] = augImage;
+      _addSphere(augImage, augImage.name);
+    }
+  }
+
+  Future _addSphere(ArCoreAugmentedImage augmentedImage, String imgName) async {
+    final material = ArCoreMaterial(
+      color: Color.fromARGB(155, 66, 134, 244),   
+      metallic: 1.0,
+    );
+
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: augmentedImage.extentX / 2,
+    );
+
+    final node = ArCoreNode(
+      shape: sphere,
+    );
+
+    arController.addArCoreNodeToAugmentedImage(node, augmentedImage.index);
+  }
+  */
+
   Alignment normalize(x, y) {
-    x = (x - 125) / 125;
+    x = (x - 100) / 100;
     x = x.toDouble();
     print("X = " + x.toString());
-    y = (y - 350) / 350;
+    y = (y - 335) / 235;
     y = y.toDouble();
     print("Y = " + y.toString());
     return Alignment(x, y);
   }
 }
+
+/* this is to display an image at a certain position, also draggable
+
+*/
+
+//this is to take a picture
+/*
+screenshotController
+                .capture(delay: Duration(milliseconds: 20))
+                .then((Uint8List image) async {
+              print(
+                  "==============================================================");
+              print(
+                  "========================Picure Taken =========================");
+              setState(() {
+                imageFile = image;
+                imageAlign = con.normalize(offset.dx, offset.dy);
+                imageOffset = offset;
+                offset = Offset(0, 100);
+                //print(imageAlign.x);
+              });
+              final result = await ImageGallerySaver.saveImage(imageFile);
+            });     
+*/
+
+/*
+RenderRepaintBoundary boundary =
+                previewContainer.currentContext.findRenderObject();
+            ui.Image image = await boundary.toImage();
+            ByteData bytes = await image.toByteData(format: ui.ImageByteFormat.png);
+            Uint8List imageByteList = bytes.buffer.asUint8List();
+            final result = await ImageGallerySaver.saveImage(imageByteList);
+*/
